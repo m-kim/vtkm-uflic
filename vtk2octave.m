0594dfc54bce4a2b3e7d8e80856b0c545b5db823
Mark Kim <mbk@cs.utah.edu>
2019-03-29 10:47:12 -0400

    works better.

--- vtk2octave.m
+++ vtk2octave.m
@@ -1,4 +1,9 @@
-[u,v] = textread("output.vtk", "%f %f %*f", "headerlines", 5);
+fid = fopen('build/debug/output.vtk')
+num = textscan(fid, '%*s %d %*s', 1, 'headerlines', 4)
+mat = textscan(fid, '%f %f %*f', num{1});
+u = mat{1};
+v = mat{2};
+fclose(fid)
 [x,y] = meshgrid(0:1.0/511.0:1, 0:1.0/511.0:1);
 u = reshape(u, 512,512);
 v = reshape(v, 512,512);
