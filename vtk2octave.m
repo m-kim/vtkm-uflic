5e3900d477f7120efb8d36e626335bff4c650502
Mark Kim <mbk@cs.utah.edu>
2019-03-31 20:08:22 -0400

    flipped and flipped.

--- vtk2octave.m
+++ vtk2octave.m
@@ -6,12 +6,16 @@ v = mat{2};
 fclose(fid)
 
 spacing = 8
-[x,y] = meshgrid(0:spacing:511, 0:spacing:511);
 uprime = reshape(u, 512,512);
 vprime = reshape(v, 512,512);
 uprime = uprime(1:spacing:end, 1:spacing:end);
 vprime = vprime(1:spacing:end, 1:spacing:end);
+uprime = flip(uprime);
+uprime = fliplr(uprime);
+vprime = flip(vprime);
+vprime = fliplr(vprime);
 f = figure('visible', 'off')
+[x,y] = meshgrid(0:spacing:511, 0:spacing:511);
 quiver(x,y,uprime,vprime);
 filename = 'screenspace.png'
 print(filename)
